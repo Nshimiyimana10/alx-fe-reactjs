758b4953f3ff13eaba5f02bd728a2a5f543620c7
NSHIMIYIMANA <joanesclaudio10@gmail.com>
2024-11-02 00:18:22 -0700

    modify UserProfile component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -37,7 +37,7 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
-      <UserProfile name="Peter" age="27" bio="I like coding" />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </>
   )
 }
