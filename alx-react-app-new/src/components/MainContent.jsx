b2ea56c3ff2bd24b15e46bfa204a25d6bb70a39a
NSHIMIYIMANA <joanesclaudio10@gmail.com>
2024-11-09 06:02:14 -0800

    added

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent(){
+    return(
+        <main>
+           <p style={{color:"white", backgroundColor:"black"}}>I love to visit New York, Paris, and Tokyo.</p>
+       </main>
+    )
+}
+
+export default MainContent
