e39d72fe0a41745ff12c78d88b6cd6dcdf111b59
NSHIMIYIMANA <joanesclaudio10@gmail.com>
2024-11-16 07:26:47 -0800

    added new files

---
+++ alx-react-app-new/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import { Outlet, Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav style={{backgroundColor:'gray',
+         display:'flex', justifyContent:'center'}}>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/services">Services</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Outlet />
+    </>
+  )
+};
+
+//This is react router
+//end of this component
