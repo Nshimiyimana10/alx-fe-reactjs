41489f188415d3e85e97b9ef2c750d4038ee9d54
NSHIMIYIMANA <joanesclaudio10@gmail.com>
2024-11-17 08:30:13 -0800

    added

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
 function UserProfile() {
+    const[context, UserContext] = useContext('Peter');
   return (
     <div>
-        <h1>This is userProfile page</h1>
+        <h1>This is userProfile page {useContext}</h1>
     </div>
   )
 }
