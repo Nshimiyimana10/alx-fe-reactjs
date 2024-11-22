931bf4b00ba6b049849c8553671cf08f736b47e2
NSHIMIYIMANA <joanesclaudio10@gmail.com>
2024-11-22 10:04:58 -0800

    added

---
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {useState} from 'react';
+
+function RegistrationForm() {
+  return (
+    <div>
+        <form action="">
+
+        </form>
+    </div>
+  )
+}
+
+export default RegistrationForm
