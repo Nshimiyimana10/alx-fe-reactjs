33cc2c6d164a3d7de73fc450e018f4c6600d65c9
NSHIMIYIMANA <joanesclaudio10@gmail.com>
2024-11-23 04:26:37 -0800

    added

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import {useState} from 'react'
 
 function PostsComponent() {
+  const [query, useQuery] = useState("");
+
+  const fetchData = ()=>{
+    const fetchPosts = "https://jsonplaceholder.typicode.com/posts"
+    const dataContainer = document.getElementById("api-data");
+    useQuery, isLoading, isError, data, error, "fetchPosts";
+    if(isLoading){
+      return "fetched successfully";
+    }
+
+    if(isError){
+       return "failed to be fetched";
+    }
+
+  }
   return (
     <div>
-        
+         
     </div>
   )
 }
