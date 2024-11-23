52c02c8154961f4cf49dcb5d5c55abd9ba245af4
NSHIMIYIMANA <joanesclaudio10@gmail.com>
2024-11-23 05:57:54 -0800

    added

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -1,5 +1,9 @@
+import Formik from 'formik'
+
 function formikForm(){
     form
     field 
     ErrorMessage;
-}
+     const[initialValues, setInitialValue ] = useState(validationSchema)
+    
+    }
