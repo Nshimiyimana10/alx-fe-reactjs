b5282a6dc7ca842c267c582a2c4e6c9e56f2b41a
NSHIMIYIMANA <joanesclaudio10@gmail.com>
2024-12-02 10:29:44 -0800

    added

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -3,9 +3,12 @@ import{useState, useEffect} from 'react';
 
 function HomePage() {
     const [data, setData] = useState("");
+    const newData = data.json.map((data) =>{
+        data.title
+    })
   return (
     <div>
-
+      
     </div>
   )
 }
