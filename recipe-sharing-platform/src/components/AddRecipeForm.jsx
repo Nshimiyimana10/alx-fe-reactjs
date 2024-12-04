6b711d37006c582baeb4546930fd257a03171dc4
NSHIMIYIMANA <joanesclaudio10@gmail.com>
2024-12-04 09:56:11 -0800

    added

--- recipe-sharing-platform/src/components/AddRecipeForm.jsx
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -3,12 +3,24 @@ import {useState} from 'react';
 
 function AddRecipeForm() {
   const [addRecipe, setAddRecipe] = useState("");
+  const newInfo =[handleSubmit];
+
+  function ingredients(e){
+   preventDefault(e.target.value);
+   const steps = "newSteps";
+   const title ="newTitle";
+  }
+
   return (
-    <div>
-       <input type="text" />
-       <textarea name="" id=""></textarea>
-       <textarea name="" id=""></textarea>
+    <div >
+    
+    <form action="" onSubmit={ingredients}>
+        <input type="text" />
+        <textarea name="" id=""></textarea>
+        <textarea name="" id=""></textarea>
        <button>SUBMIT</button>
+    </form>
+       
     </div>
   )
 }
